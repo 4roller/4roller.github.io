a603061bc0796de2d342c5e039e3a04a0ad71711
Jason Leung <closednode@gmail.com>
2022-02-10 18:52:20 -0800

    [tuner] adding more parts

---
+++ subaru-impreza-tuner/assets/js/drivetrainGain.js
@@ -0,0 +1,11 @@
+const drivetrainGain = (function(){
+    return {
+        init: function() {
+            console.log('Drivetrain Gain Simulator');
+        }
+    }
+})();
+
+window.onload = function(){
+	drivetrainGain.init();	
+};
