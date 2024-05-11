be957d9fee072c168b8c88d6228507d0279f1207
zero <closednode@gmail.com>
2024-05-10 18:51:41 -0700

    [soundboard] adding triple buffer

--- SoundBoard/Gundam/sound-board.js
+++ SoundBoard/Gundam/sound-board.js
@@ -1,6 +1,6 @@
 var SoundBoard = (function() {
 
-	var d, ap0, ap1, buttonWrapper, ap0Status, ap1Status;
+	var d, ap0, ap1, ap2, buttonWrapper, ap0Status, ap1Status;
 	var addListeners = function() {
 		console.log('Adding listeners ...');
 		buttonWrapper.addEventListener('click', function(e) {
@@ -8,12 +8,14 @@ var SoundBoard = (function() {
 						
 			if(ap0Status !== 'playing') {
 				ap0.src = 'sounds/' + e.target.getAttribute('data-src');
-				ap0.play();	
-			} else {
+				ap0.play();			
+			} else if (ap1Status !== 'playing') {
 				ap1.src = 'sounds/' + e.target.getAttribute('data-src');
 				ap1.play();
-			}
-			
+			} else {
+				ap2.src = 'sounds/' + e.target.getAttribute('data-src');
+				ap2.play();
+			}			
 		});
 
 		ap0.addEventListener('playing', function(e) {
@@ -21,6 +23,12 @@ var SoundBoard = (function() {
 		});
 		ap0.addEventListener('ended', function(e) {
 			ap0Status = 'stopped';
+		});
+		ap1.addEventListener('playing', function(e) {
+			ap1Status = 'playing';			
+		});
+		ap1.addEventListener('ended', function(e) {
+			ap1Status = 'stopped';
 		});
 
 	}
@@ -30,9 +38,9 @@ var SoundBoard = (function() {
 			console.log('Sound Board initializing ...');
 			
 			d = document;
-			ap0 = d.getElementById('ap0');
-			
+			ap0 = d.getElementById('ap0');			
 			ap1 = d.getElementById('ap1');
+			ap2 = d.getElementById('ap2');
 			buttonWrapper = d.querySelector('.button-wrapper');
 			addListeners();
 		}
